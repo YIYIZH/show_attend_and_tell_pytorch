dd099f43811a5f59a8bb88c38f4f77110e9b9f32
ZHANG Yiyi <yiyimonica@gmail.com>
2019-03-05 11:24:36 +0800

    Update prepro.py
    modify paths

--- prepro.py
+++ prepro.py
@@ -85,11 +85,11 @@ def main(args):
     print("Start resize_image")
     splits = ['train', 'val']
     for split in splits:
-        folder = './image/%s2014' %split
-        resized_folder = './image/%s2014_resized/' %split
+        folder = '/data1/public/coco/data/%s2014' %split
+        resized_folder = '/data1/public/coco/data/%s2014_resized/' %split
         if not os.path.exists(resized_folder):
             os.makedirs(resized_folder)
-        print 'Start resizing %s images.' %split
+        print ('Start resizing %s images.' %split)
         image_files = os.listdir(folder)
         num_images = len(image_files)
         for i, image_file in enumerate(image_files):
@@ -98,13 +98,13 @@ def main(args):
                     image = resize_image(image)
                     image.save(os.path.join(resized_folder, image_file), image.format)
             if i % 100 == 0:
-                print 'Resized images: %d/%d' %(i, num_images)
+                print ('Resized images: %d/%d' %(i, num_images))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--caption_path', type=str,
-                        default='/usr/share/mscoco/annotations/captions_train2014.json',
+                        default='/data1/public/coco/annotation/instances_train2014.json',
                         help='path for train annotation file')
     parser.add_argument('--vocab_path', type=str, default='./data/vocab.pkl',
                         help='path for saving vocabulary wrapper')
