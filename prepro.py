d0c91e98a2adf01d4a0c6ec78fe9a2a06be97c5e
ZHANG Yiyi <yiyimonica@gmail.com>
2019-03-05 15:47:44 +0800

    modify path

--- prepro.py
+++ prepro.py
@@ -104,7 +104,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--caption_path', type=str,
-                        default='/data1/public/coco/captions_train2014.json',
+                        default='/data1/public/coco/annotations/captions_train2014.json',
                         help='path for train annotation file')
     parser.add_argument('--vocab_path', type=str, default='./data/vocab.pkl',
                         help='path for saving vocabulary wrapper')
